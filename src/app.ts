2cf9c0572b239a44f741eee241da4b0fb41d9f8b
Muthu Palaniyappan OL <muthu892542@gmail.com>
2024-11-21 07:54:00 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 function targetFunction() {
     const activeSheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet()
     const range = activeSheet.getRange(1, 1)
-    range.setValue("Push from here")
+    range.setValue("Pushing After 2 Weeks !! ")
 }
 
 targetFunction()
